886da06268c3bcb83ed5edd2c4c5a2bac193b6ce
Luciefur <luciefur6393473659@gmail.com>
2024-11-13 20:41:16 +0530

    Added DashBoard Setting Page Components - Update Password

--- Backend/routes/profile.routes.js
+++ Backend/routes/profile.routes.js
@@ -3,7 +3,9 @@ import { auth } from "../middlewares/auth.middleware.js";
 import {
 	deleteAccount,
 	getAllUserDetails,
+	getEnrolledCourses,
 	updateProfile,
+	updateProfilePicture,
 } from "../controllers/profile.controller.js";
 
 const profileRouter = Router();
@@ -11,5 +13,7 @@ const profileRouter = Router();
 profileRouter.delete("/deleteProfile", auth, deleteAccount);
 profileRouter.put("/updateProfile", auth, updateProfile);
 profileRouter.get("/getUserDetails", auth, getAllUserDetails);
+profileRouter.put("/updateProfilePicture", auth, updateProfilePicture);
+profileRouter.get("/getEnrolledCourses", auth, getEnrolledCourses);
 
 export default profileRouter;
