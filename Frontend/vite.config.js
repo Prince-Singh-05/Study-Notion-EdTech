bcfe6feaf739863f42917c6ef2ebad16a7f4ebe0
Luciefur <luciefur6393473659@gmail.com>
2025-01-11 14:26:56 +0530

    added frontend url for cors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		outDir: "dist",
+		sourcemap: false,
+	},
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: process.env.VITE_BASE_URL,
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+});
