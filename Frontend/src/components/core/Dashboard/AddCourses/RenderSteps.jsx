bf71358adacbb773b61852baa4dd3e2de23368f8
Luciefur <luciefur6393473659@gmail.com>
2025-03-02 21:55:02 +0530

    updated addCourse

--- Frontend/src/components/core/Dashboard/AddCourses/RenderSteps.jsx
+++ Frontend/src/components/core/Dashboard/AddCourses/RenderSteps.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { FaCheck } from "react-icons/fa6";
 import { useSelector } from "react-redux";
-import CourseInformationForm from "./CourseInformationForm";
+import CourseInformationForm from "./CourseInformation/CourseInformationForm";
+import CourseBuilderForm from "./CourseBuilder/CourseBuilderForm";
+import PublishCourse from "./PublishCourse";
 
 const RenderSteps = () => {
 	const { step } = useSelector((state) => state.course);
@@ -9,7 +11,7 @@ const RenderSteps = () => {
 	const steps = [
 		{
 			id: 1,
-			title: "Course Imformation",
+			title: "Course Information",
 		},
 		{
 			id: 2,
@@ -23,33 +25,35 @@ const RenderSteps = () => {
 
 	return (
 		<>
-			<div>
+			<div className="flex justify-between xl:w-[70%] w-full mx-auto">
 				{steps.map((item) => (
-					<div key={item.id}>
+					<div key={item.id} className="flex-1">
 						<div
-							className={`${
-								step === item.id
-									? "bg-yellow-900 border-yellow-50 text-yellow-50"
-									: "border-e-richblack-700 bg-richblack-800 text-richblack-300"
-							}`}
+							className={`flex flex-col gap-2 text-center justify-center`}
 						>
-							{step > item.id ? <FaCheck /> : item.id}
+							<span
+								className={`${
+									step === item.id
+										? "bg-yellow-900 border-yellow-50 border text-yellow-50"
+										: "border-e-richblack-700 bg-richblack-800 text-richblack-300"
+								} rounded-full w-10 h-10 px-4 py-2 mx-auto flex items-center`}
+							>
+								{step > item.id ? (
+									<FaCheck width={20} height={20} />
+								) : (
+									item.id
+								)}
+							</span>
+							<span>{item.title}</span>
 						</div>
 						{/* Dashes between label */}
 					</div>
 				))}
 			</div>
-			<div>
-				{steps.map((item) => (
-					<div key={item.id}>
-						<p>{item.title}</p>
-					</div>
-				))}
-			</div>
 
 			{step === 1 && <CourseInformationForm />}
-			{/* {step === 2 && <CourseBuilderForm />} */}
-			{/* {step === 3 && <PublishCourse />} */}
+			{step === 2 && <CourseBuilderForm />}
+			{step === 3 && <PublishCourse />}
 		</>
 	);
 };
--- Frontend/src/components/core/Dashboard/AddCourses/index.jsx
+++ Frontend/src/components/core/Dashboard/AddCourses/index.jsx
@@ -3,36 +3,39 @@ import RenderSteps from "./RenderSteps";
 
 const AddCourse = () => {
 	return (
-		<div className="text-richblack-5">
-			<div>
-				<h1>Add Course</h1>
-
-				<div>
+		<div className="text-richblack-5 flex flex-col p-6 justify-center gap-4">
+			<h1 className="font-semibold text-3xl">Add Course</h1>
+			<div className="w-full flex gap-10 xl:pl-24 transition-all duration-1000">
+				<div className="w-full">
 					<RenderSteps />
 				</div>
-			</div>
 
-			<div>
-				<h2>Code Upload Tips</h2>
-				<ul>
-					<li>Set the Course Price option or make it free.</li>
-					<li>Standard size for the course thumbnail is 1024x576.</li>
-					<li>Video section controls the course overview video.</li>
-					<li>
-						Course Builder is where you create and organize a
-						course.
-					</li>
-					<li>
-						Add Topics in the Course Builder section to create
-						lessons, quizzes, and assignments.
-					</li>
-					<li>
-						Information from the Additional Data section shows up on
-						the course single page.
-					</li>
-					<li>Make Announcements to notify any important.</li>
-					<li>Notes to all enrolled students at once.</li>
-				</ul>
+				<div className="xl:block hidden">
+					<h2>Code Upload Tips</h2>
+					<ul>
+						<li>Set the Course Price option or make it free.</li>
+						<li>
+							Standard size for the course thumbnail is 1024x576.
+						</li>
+						<li>
+							Video section controls the course overview video.
+						</li>
+						<li>
+							Course Builder is where you create and organize a
+							course.
+						</li>
+						<li>
+							Add Topics in the Course Builder section to create
+							lessons, quizzes, and assignments.
+						</li>
+						<li>
+							Information from the Additional Data section shows
+							up on the course single page.
+						</li>
+						<li>Make Announcements to notify any important.</li>
+						<li>Notes to all enrolled students at once.</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
